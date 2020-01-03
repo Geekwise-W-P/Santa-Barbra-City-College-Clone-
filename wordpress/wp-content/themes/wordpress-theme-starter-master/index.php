93de6ecd2c4c53331344e43ba4a996865a0b8d27
Natalie <natalias01@gmail.com>
2020-01-02 16:33:57 -0800

    Got boostrap carousel to work, need to fix positioning

--- wordpress/wp-content/themes/wordpress-theme-starter-master/index.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/index.php
@@ -1,32 +1,33 @@
 <?php get_header(); ?>
-<div class="container-fluid px-0">
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner bg-info" role="listbox">
-            <div class="carousel-item active">
-                <div class="d-flex align-items-center justify-content-center min-vh-100">
-                    <h1 class="display-1">ONE</h1>
-                </div>
-            </div>
-            <dixv class="carousel-item">
-                <div class="d-flex align-items-center justify-content-center min-vh-100">
-                    <h1 class="display-1">TWO</h1>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <div class="d-flex align-items-center justify-content-center min-vh-100">
-                    <h1 class="display-1">THREE</h1>
-                </div>
-            </div>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+
+  <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img class="d-block w-100" src="http://SBCC.clone/wp-content/uploads/2020/01/sb-carousel1.jpg" alt="First slide">
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
+        <div class="carousel-item">
+        <img class="d-block w-100" src="http://SBCC.clone/wp-content/uploads/2020/01/sb-carousel2.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+        <img class="d-block w-100" src="http://SBCC.clone/wp-content/uploads/2020/01/sb-carousel3.jpg" alt="Third slide">
+        </div>
+  </div>
+  
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
 
         <div class="container">
